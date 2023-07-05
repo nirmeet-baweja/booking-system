e765f703bf82936fb5ecfceb4ba57910f65ce489
nirmeet-baweja <73722230+nirmeet-baweja@users.noreply.github.com>
2023-07-05 21:46:20 +0100

    update app to fetch message from server

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,25 @@
+import { useEffect, useState } from "react";
+
 const App = () => {
+  const [message, setMessage] = useState("Some initial message");
+
+  // Fetching message from backend on mount
+  useEffect(() => {
+    const fetchMessage = async () => {
+      const requestUrl = process.env.REACT_APP_SERVER_URL;
+      console.log("requestUrl");
+      console.log(requestUrl);
+      const response = await fetch(requestUrl);
+      const data = await response.json();
+      setMessage(data.message);
+    };
+
+    fetchMessage();
+  }, []);
+
   return (
-    <div>
-      <h1>Booking System - Client Side App</h1>
+    <div className="App">
+      <h1>{message}</h1>
     </div>
   );
 };
